1a800403144a980675f1301012617b7bbe8af800
cnicoloff <cnicolof@wellesley.edu>
2018-04-29 08:52:34 -0400

    Fixed another bug related to changing function names

--- star.c
+++ star.c
@@ -107,7 +107,7 @@ int main (void)
     P = readPUncompensated();
     T2 = roundPrecision(calcFirstOrderT(T), 2);
     P2 = roundPrecision(calcSecondOrderP(T, P), 2);
-    alt = roundPrecision(getAltitude(P2, T2), 1);
+    alt = roundPrecision(calcAltitude(P2, T2), 1);
     // Write some output
     printf("uSv/hr: %2.2f, T: %3.2f C (%3.2f F), P: %4.2f mbar, h: %7.2f m\n", uSv, T2, cvtCtoF(T2), P2, alt);
   }

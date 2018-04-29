6d7f7ae226a03ce78a531c1f9715b8066d0f577a
cnicoloff <cnicolof@wellesley.edu>
2018-04-29 14:01:16 -0400

    Remove debug output

--- geiger.c
+++ geiger.c
@@ -186,8 +186,6 @@ int sumCounts(int numSecs) {
   int numMins = ((numSecs - (numHours * 3600)) / 60) % size;        // Seconds to minutes
   numSecs = (numSecs - (numMins * 60) - (numHours * 3600)) % size;  // Remaining seconds
   
-  printf("sumCounts: %02d:%02d:%02d\n", numHours, numMins, numSecs);
-
   // Sum hours
   for (int i=0; i < numHours; i++) {
     total += hour[getIndex(hourNum - i)];
